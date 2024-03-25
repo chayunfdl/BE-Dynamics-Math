76880d69ad10fabeb279e710bd6beb39bd6b9085
Chayun Fadila <128894491+chayunfdl@users.noreply.github.com>
2024-03-25 07:59:17 +0700

    finaliazing rest api

--- src/app.js
+++ src/app.js
@@ -26,9 +26,9 @@ app.use(cors({
 
 app.use(`${globalApiPrefix}/`,
 	express.Router()
-		.use('/api/auth', AuthRouter)
-		.use('/api/user', UserRouter)
-		.use('/api/contact', ContactRouter)
+		.use("/auth", AuthRouter)
+		.use("/user", UserRouter)
+		.use("/contact", ContactRouter)
 );
 
 
